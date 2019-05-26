a36eecda9319b3ed720da50ec67316870d06b04b
valeria <tito_alba@mail.ru>
2019-05-26 11:39:30 +0300

    db template

---
+++ db_maker/db_maker.py
@@ -0,0 +1,28 @@
+from db_utils import Database
+
+
+def make_tables():
+    db = Database()
+    db.execute("DROP TABLE IF EXISTS word_info;", 0)
+    db.execute("""CREATE TABLE word_info
+                    (word_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    word TEXT, 
+                    definition TEXT,
+                    updater TEXT);
+                           """, 0)
+    db.execute("DROP TABLE IF EXISTS examples;", 0)
+    db.execute("""CREATE TABLE examples
+                    (word_id INTEGER, 
+                    example TEXT);
+                            """, 0)
+    db.execute("DROP TABLE IF EXISTS updaters;", 0)
+    db.execute("""CREATE TABLE updaters
+                    (author_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    city TEXT,
+                    date DATE);
+                            """, 0)
+    db.commit()
+
+
+if __name__ == '__main__':
+    make_tables()
---
+++ db_maker/db_utils.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+NAME = 'i_dict_db'
+
+
+class Database:
+    def __init__(self):
+        self._connection = sqlite3.connect(NAME)
+
+    def commit(self):
+        self._connection.commit()
+
+    def execute(self, q, arg):
+        cur = self._connection.cursor()
+        if arg != 0:
+            cur.execute(q, arg)
+        else:
+            cur.execute(q)
+        res = cur.fetchall()
+        cur.close()
+        return res
